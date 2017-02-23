b2e911bbc43a4c6f510865e6d4fb440849f6a783
immanuelfodor <immanuel.fodor@codecool.com>
2017-02-23 17:59:06 +0100

    the prime function skeleton added to main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+def count_primes(n):
+    """
+    Counts the number of prime numbers less than a non-negative
+    number (n) by using the sieve of Eratosthenes.
+    
+    >>> count_primes(1)
+    0
+    >>> count_primes(2)
+    0
+    >>> count_primes(3)
+    1
+    >>> count_primes(13)
+    5
+    >>> count_primes(10000)
+    1229
+    
+    :see: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+    :param n: int
+    :returns: int
+    """
+    
+    return 0
+    
